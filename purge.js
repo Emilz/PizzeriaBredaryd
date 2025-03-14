e9edb753c2d46c17e39b2fa7c39c5cb993789355
Emilz <Emilz@users.noreply.github.com>
2025-03-14 19:50:33 +0100

    Remove purify-css. Replace with purgecss and clean-css

---
+++ purge.js
@@ -0,0 +1,28 @@
+const PurgeCSS = require('purgecss').PurgeCSS;
+const CleanCSS = require('clean-css');
+const fs = require('fs');
+
+const content = ['index.html'];
+const css = ['css/bootstrap.min.css', 'css/slide.css', 'css/style.css'];
+const output = 'css/styles.min.css';
+
+(async () => {
+    const purgeCSSResult = await new PurgeCSS().purge({
+        content: content,
+        css: css,
+        safelist: {
+            standard: ['slidesjs'],
+            deep: [/^#slides/, /slidesjs/]
+        }
+    });
+
+    const combinedCSS = purgeCSSResult.map(result => result.css).join("\n");
+
+    const minifiedCSS = new CleanCSS().minify(combinedCSS).styles;
+
+    fs.writeFileSync(output, minifiedCSS);
+
+    console.log(`Purged and minified CSS saved to: ${output}`);
+})();
+
+//run: node purge.js
